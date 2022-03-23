dc97df29f79f870ad4e273a40b62f08aaa320c06
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-23 16:23:03 +0100

    myCnavas

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -10,33 +10,7 @@ public class Main {
         okno.setSize(300,300);
         okno.setResizable(true);
         okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //toto treba na zadani urcite!
-        okno.setLayout(new GridLayout(3,4));
 
-        JPanel panel = new JPanel();
-        panel.setLayout(new BorderLayout(5,5));
-
-        panel.add(new JLabel("TOP"),BorderLayout.PAGE_START);
-        panel.add(new JLabel("BOTTOM"),BorderLayout.PAGE_END);
-        panel.add(new JLabel("LEFT"),BorderLayout.LINE_START);
-        panel.add(new JLabel("RIGHT"),BorderLayout.LINE_END);
-        JLabel labelpanel = new JLabel("CENTER",SwingConstants.CENTER);
-        labelpanel.setAlignmentX(150);
-
-        panel.add(labelpanel,BorderLayout.CENTER);
-
-        panel.setBackground(new Color(190,120,150));
-        okno.getContentPane().setBackground(Color.YELLOW);
-
-        JLabel label = new JLabel("Hello world");
-        okno.add(label);
-
-        for (int i=0;i<10;i++){
-            if (i==8){
-                okno.add(panel);
-            }else{
-                okno.add(new JLabel("Hello "+i));
-            }
-
-        }
+        okno.add(new MyCanvas());
     }
 }
