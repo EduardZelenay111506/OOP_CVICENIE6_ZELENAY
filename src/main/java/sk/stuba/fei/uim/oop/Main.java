592cce0396ec8b09df76704f894fb268d131fb29
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-23 15:31:29 +0100

    zaklad enum

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -2,6 +2,19 @@ package sk.stuba.fei.uim.oop;
 
 public class Main {
     public static void main(String[] args) {
+        SvetoveStrany strana = SvetoveStrany.SEVER;
 
+        System.out.println(SvetoveStrany.valueOf("SEVER"));
+        for(SvetoveStrany s :SvetoveStrany.values()){
+            checkSever(s);
+        }
     }
+     public static void checkSever(SvetoveStrany s){
+         if (s.equals("SEVER")){
+             System.out.println("sever");
+         }
+         else{
+             System.out.println("neni sever");
+         }
+     }
 }
