5691fb5570ee946ea923967d237cfe31041da521
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-23 15:39:39 +0100

    enum continues

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -4,17 +4,26 @@ public class Main {
     public static void main(String[] args) {
         SvetoveStrany strana = SvetoveStrany.SEVER;
 
-        System.out.println(SvetoveStrany.valueOf("SEVER"));
+        System.out.println(SvetoveStrany.fromString("sever"));
         for(SvetoveStrany s :SvetoveStrany.values()){
             checkSever(s);
         }
+        switch (strana){
+            case SEVER:System.out.println("sever");
+            break ;
+            default:System.out.println("neni sever");
+            break;
     }
+    }
+
      public static void checkSever(SvetoveStrany s){
-         if (s.equals("SEVER")){
+         if (s.isJeSever()){
              System.out.println("sever");
          }
          else{
              System.out.println("neni sever");
          }
      }
-}
+
+    }
+
